ed0e468642a04dd2bb44a424eb07806a057eaf8b
Kathleen <32270467+kcervania@users.noreply.github.com>
2021-09-07 22:10:53 -0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,31 +1,83 @@
+
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// 1. Get HTML Elements
+// * * * * * * * * * * * * * * * * * * * * * * * * * * *
+var openButton      = document.querySelector('#open-mobile-menu-button');
+var closeButton     = document.querySelector('#close-mobile-menu-button');
+var mobileMenu      = document.querySelector('#mobile-menu');
+
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// 2. Define Abilities (Functions)
+// * * * * * * * * * * * * * * * * * * * * * * * * * * *
+function openMenu() {
+    mobileMenu.className = 'menu open';
+}
+
+function closeMenu() {
+    mobileMenu.className = 'menu close';
+}
+
+
 $('a').click(function(){
     $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top()
+        scrollTop: $( $(this).attr('href') ).offset().top
     }, 1000);
     return false;
 });
 
-var slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
 
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
+// * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// 3. Wire everything up
+// * * * * * * * * * * * * * * * * * * * * * * * * * * *
+openButton.addEventListener('click', openMenu);
+closeButton.addEventListener('click', closeMenu);
 
-  slides[slideIndex-1].style.display = "block";  
+// * * * * * * * * * * * * * * * * * * * * * * * * * * *
+//Testimonials
+// * * * * * * * * * * * * * * * * * * * * * * * * * * *
+$('.responsive').slick({
+  dots: true,
+	prevArrow: $('.prev'),
+	nextArrow: $('.next'),
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    }
+  ]
+});
 
-}
